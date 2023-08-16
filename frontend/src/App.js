9e28d234e4c0d6358c64a9147895976e2cb07dae
Andersen Sven <andersensven.star@gmail.com>
2023-08-16 08:13:06 +0200

    initialized frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <button
+        className="btn btn-primary btn-lg mx-3 px-5 py-3 mt-2"
+      >
+        Add
+      </button>
+    </div>
+  );
+}
+
+export default App;
