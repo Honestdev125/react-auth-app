6a19cf5a983ecb51e989485cdba91d7d6a64d842
Andersen Sven <andersensven.star@gmail.com>
2023-08-18 03:42:48 +0200

    implement the signin form

--- frontend/src/components/Navbar/NavItem.js
+++ frontend/src/components/Navbar/NavItem.js
@@ -4,7 +4,7 @@ const NavItem = ({ name, href }) => {
   return (
     <Link
       key={name}
-      href={href}
+      to={href}
       className='text-custom hover:bg-gray-700 hover:text-white rounded-md px-2 md:px-3 py-3 text-sm font-medium'
     >
       {name}
