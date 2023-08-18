7e4cdcf6081de2f64ed455455924cda9397ea747
Andersen Sven <andersensven.star@gmail.com>
2023-08-18 11:24:50 +0200

    fix the register

--- backend/validation/register.js
+++ backend/validation/register.js
@@ -4,27 +4,27 @@ const isEmpty = require("./is-empty");
 module.exports = function validateRegisterInput(data) {
   let errors = {};
 
-  data.firstname = !isEmpty(data.firstname) ? data.firstname : "";
-  data.lastname = !isEmpty(data.lastname) ? data.lastname : "";
+  data.firstName = !isEmpty(data.firstName) ? data.firstName : "";
+  data.lastName = !isEmpty(data.lastName) ? data.lastName : "";
   data.email = !isEmpty(data.email) ? data.email : "";
   data.email2 = !isEmpty(data.email2) ? data.email2 : "";
   data.password = !isEmpty(data.password) ? data.password : "";
   data.password2 = !isEmpty(data.password2) ? data.password2 : "";
 
-  if (!Validator.isLength(data.firstname, { min: 2, max: 30 })) {
-    errors.firstname = "Name must be between 2 and 30 characters";
+  if (!Validator.isLength(data.firstName, { min: 2, max: 30 })) {
+    errors.firstName = "Name must be between 2 and 30 characters";
   }
 
-  if (Validator.isEmpty(data.firstname)) {
-    errors.firstname = "Name field is required";
+  if (Validator.isEmpty(data.firstName)) {
+    errors.firstName = "Name field is required";
   }
 
-  if (!Validator.isLength(data.lastname, { min: 2, max: 30 })) {
-    errors.lastname = "Name must be between 2 and 30 characters";
+  if (!Validator.isLength(data.lastName, { min: 2, max: 30 })) {
+    errors.lastName = "Name must be between 2 and 30 characters";
   }
 
-  if (Validator.isEmpty(data.lastname)) {
-    errors.lastname = "Name field is required";
+  if (Validator.isEmpty(data.lastName)) {
+    errors.lastName = "Name field is required";
   }
 
   if (Validator.isEmpty(data.email)) {
